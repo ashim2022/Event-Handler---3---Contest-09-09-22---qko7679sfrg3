b6a4ecfdab8e34724939cde00bed1fa6c65b98fe
Ashim Shaw <shawashim72@gmail.com>
2022-09-11 21:16:48 +0530

    files 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,62 +1,50 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/App.css";
 const App = () => {
   const [inputValueText, setInputValueText] = useState("");
-  const [itemText, setItemText] = useState([]);
-  const [inputValueNumber, setInputValueNumber] = useState("");
-  const [itemNumber, setItemNumber] = useState([]);
+  const [inputValueNumber, setInputValueNumber] = useState(0);
 
   const handleInput = (event) => {
-    // use console.log
-    setInputValueText(event.target.value);
-    setItemText((oldItemText) => {
-      return [...oldItemText, inputValueText];
-    });
-    setInputValueNumber(event.target.value);
-    setItemNumber((oldItemNumber) => {
-      return [...oldItemNumber, inputValueNumber];
-    });
+    switch (event.target.id) {
+      case "text-input": {
+        setInputValueText(event.target.value);
+        break;
+      }
+      case "num-input": {
+        setInputValueNumber(event.target.value);
+        break;
+      }
+      default: {
+        return;
+      }
+    }
   };
-  // function handleOnChange1(e) {
-  //   setInputValueText(e.target.value);
-  // }
-  // function handleOnChange2(event) {
-  //   setInputValueNumber(event.target.value);
-  // }
-  // do not change id of input elements
+
+  useEffect(() => {
+    console.log("Input in #text-input is ", inputValueText);
+  }, [inputValueText]);
+
+  useEffect(() => {
+    console.log("Input in #num-input is ", inputValueNumber);
+  }, [inputValueNumber]);
+
   return (
     <div id="main">
       <label htmlFor="text-input">Text Input:- </label>
       <input
         id="text-input"
         type="text"
-        value="inputValueText"
+        value={inputValueText}
         onChange={handleInput}
-        // onChange={handleOnChange1}
       />
-      {/* type={"text"} */}
-      <br />
-      <ul>
-        {itemText.map((value1) => {
-          return console.log(value1);
-        })}
-      </ul>
       <br />
       <label htmlFor="num-input">Number input</label>
       <input
         id="num-input"
         type="number"
-        value="inputValueNumber"
+        value={inputValueNumber}
         onChange={handleInput}
-        // onChange={handleOnChange2}
       />
-      <ul>
-        {itemNumber.map((value2) => {
-          return console.log(value2);
-        })}
-      </ul>
-      {/* type={"number"} */}
-      <br />
     </div>
   );
 };
